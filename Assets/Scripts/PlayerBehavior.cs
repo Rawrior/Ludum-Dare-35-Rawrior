fcb7ebc47b729f6f54f116c437f747f5d9e9b712
Simon Vinberg Andersen <mr.simon3500@gmail.com>
2016-04-16 11:06:02 +0200

    Physics movement worked like shit. I'll figure it out some other day. Player can move side to side.

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour
+{
+	public float Speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		MoveMethod(KeyCode.A, Vector2.left);
+		MoveMethod(KeyCode.D, Vector2.right);
+	}
+
+	void MoveMethod(KeyCode key, Vector2 direction)
+	{
+		if (Input.GetKey(key))
+		{
+			transform.Translate(direction*Speed*Time.deltaTime);
+		}
+	}
+}
